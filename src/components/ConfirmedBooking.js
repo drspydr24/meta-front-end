b2982b844f277e7b4d6a2f4adf6ec70c33ca60f1
drspydr24 <drspydr24@hotmail.com>
2025-04-07 12:29:17 -0400

    Add files via upload

---
+++ src/components/ConfirmedBooking.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const  confirmedBooking = () =>{
+    return(
+<div className ='confirm'>
+    <div>
+        <h1>Booking has been <span>confirmed!</span></h1>
+    </div>
+</div>
+
+    )
+}
+export default confirmedBooking;
